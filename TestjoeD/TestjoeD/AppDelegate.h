9362e4247b10549009971d2f3c7bb2f0a2daf9cf
joe <joe@techemstudios.com>
2017-07-31 07:03:42 -0400

    Adding 3 and 4

---
+++ TestjoeD/TestjoeD/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Testjoe D
+//
+//  Created by Joe seiler on Monday, July 31, 2017
+//  Copyright (c) Joe seiler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class StandaloneCodeaViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) StandaloneCodeaViewController *viewController;
+
+@end
