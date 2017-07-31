4de866285847a03eb88e53c1582faeece4938fcb
joe <joe@techemstudios.com>
2017-07-31 07:01:49 -0400

    Adding with unzipped

---
+++ TestjoeB/TestjoeB/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Testjoe B
+//
+//  Created by Joe seiler on Monday, July 31, 2017
+//  Copyright (c) Joe seiler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
