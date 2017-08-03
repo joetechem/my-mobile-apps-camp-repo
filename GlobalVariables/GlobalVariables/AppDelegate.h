5e3057f675ebc4da06234ae09f47b791b8bde997
joe <joe@techemstudios.com>
2017-08-03 13:25:23 -0400

    My first codea project

---
+++ GlobalVariables/GlobalVariables/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Global Variables
+//
+//  Created by Joe Seiler on Thursday, August 3, 2017
+//  Copyright (c) Joe Seiler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class StandaloneCodeaViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) StandaloneCodeaViewController *viewController;
+
+@end
