775d4ebd769a83883a1ac00233510e1593b397c1
joe <joe@techemstudios.com>
2017-08-03 12:59:17 -0400

    Test aug 3

---
+++ JoeGlobalVariables/JoeGlobalVariables/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Joe Global Variables
+//
+//  Created by Joe seiler on Thursday, August 3, 2017
+//  Copyright (c) Joe seiler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class StandaloneCodeaViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) StandaloneCodeaViewController *viewController;
+
+@end
---
+++ JoeGlobalVariables/JoeGlobalVariables/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Joe Global Variables
+//
+//  Created by Joe seiler on Thursday, August 3, 2017
+//  Copyright (c) Joe seiler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
